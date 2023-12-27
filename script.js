599a96bf4d82cf370242bf449ed924fe221ce1ec
chartaseus <90235896+chartaseus@users.noreply.github.com>
2023-12-27 15:57:40 +0700

    Store grid size in a variable

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const container = document.querySelector(".grid-container");
-for (let i = 0; i < (16 * 16); i++) {
+let gridSize = 16;
+for (let i = 0; i < (gridSize * gridSize); i++) {
   const cell = document.createElement("div");
   cell.classList.add("cell");
+  cell.style.cssText = `width: ${600/gridSize}px; height: ${600/gridSize}px`;
   container.appendChild(cell);
 }
