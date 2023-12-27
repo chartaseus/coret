d4eb31223aeb68b0ae58b1e05015a4b707dc1ea5
chartaseus <90235896+chartaseus@users.noreply.github.com>
2023-12-27 11:29:29 +0700

    Create 16x16 grid

---
+++ script.js
@@ -0,0 +1,6 @@
+const container = document.querySelector(".grid-container");
+for (let i = 0; i < (16 * 16); i++) {
+  const cell = document.createElement("div");
+  cell.classList.add("cell");
+  container.appendChild(cell);
+}
