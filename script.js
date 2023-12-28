4a146f10f051ac46ca28e73d5e2bafe45ceb30dc
chartaseus <90235896+chartaseus@users.noreply.github.com>
2023-12-28 11:56:24 +0700

    Wrap the code for creating grid in a function

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const container = document.querySelector(".grid-container");
-let gridSize = 16;
-for (let i = 0; i < (gridSize * gridSize); i++) {
-  const cell = document.createElement("div");
-  cell.classList.add("cell");
-  cell.style.cssText = `width: ${600/gridSize}px; height: ${600/gridSize}px`;
-  container.appendChild(cell);
+
+function createGrid(gridSize = 16) {
+  for (let i = 0; i < (gridSize * gridSize); i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    cell.style.cssText = `width: ${600/gridSize}px; height: ${600/gridSize}px`;
+    container.appendChild(cell);
+  }
 }
+
+createGrid();
 
 const cells = document.querySelectorAll(".cell");
 cells.forEach((cell) => {
