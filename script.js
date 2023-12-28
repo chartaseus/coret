4d538ef1ecfd1313282f0c4c25b26e4d27fd7ad3
chartaseus <90235896+chartaseus@users.noreply.github.com>
2023-12-28 13:17:58 +0700

    Add button to change grid size
    Cell mousenter event didn't work after grid size is changed, so I moved
    the mousenter event into the createGrid function.

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 const container = document.querySelector(".grid-container");
+const changeGridButton = document.getElementById("changeSize");
+changeGridButton.addEventListener("click", changeGrid);
 
 function createGrid(gridSize = 16) {
   for (let i = 0; i < (gridSize * gridSize); i++) {
     const cell = document.createElement("div");
     cell.classList.add("cell");
-    cell.style.cssText = `width: ${600/gridSize}px; height: ${600/gridSize}px`;
+    cell.style.cssText = `width: ${600 / gridSize}px; height: ${600 / gridSize}px`;
     container.appendChild(cell);
   }
+  const cells = document.querySelectorAll(".cell");
+  cells.forEach((cell) => {
+    cell.addEventListener("mouseenter", () => {
+      cell.style.backgroundColor = "black";
+    })
+  });
+}
+
+function changeGrid() {
+  while (container.hasChildNodes()) {
+    container.removeChild(container.firstChild);
+  }
+  let gridSize = parseInt(prompt("Enter number of squares you want in a row (up to 100)"));
+  createGrid(gridSize);
 }
 
 createGrid();
-
-const cells = document.querySelectorAll(".cell");
-cells.forEach((cell) => {
-  cell.addEventListener("mouseenter", () => {
-    cell.style.backgroundColor = "black";
-  })
-});
